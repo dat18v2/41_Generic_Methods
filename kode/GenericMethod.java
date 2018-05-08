98ae72727c5df50ae6adf8106d23324fb6589405
JarlTuxen <jart@kea.dk>
2018-05-08 08:59:10 +0200

    generic serialization

--- kode/GenericMethod.java
+++ kode/GenericMethod.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.io.*;
+
 public class GenericMethod {
    // Inspired by Tutorialspoint
    
@@ -8,10 +10,37 @@ public class GenericMethod {
          System.out.printf("%s ", element);
       }
    }
+   
+   public static < E > void writeArrayList ( ArrayList<E> inputArrayList) throws IOException {
+      // write arraylist to disk
+      // åbn inputstreams på al.ser
+      FileOutputStream fo = new FileOutputStream("al.ser");
+      ObjectOutputStream oo = new ObjectOutputStream(fo);
+      // serialiser arraylist af generisk type til fil
+      oo.writeObject(inputArrayList);
+      // luk inputstreams
+      oo.close();
+      fo.close();
+   }
+   
+   public static < E > ArrayList<E> readArrayList () throws IOException, ClassNotFoundException{
+      // read arraylist from disk
+      // ArrayList til at returnere
+      ArrayList<E> outputArrayList = new ArrayList<E>();
+      // åbn inputstreams på al.ser
+      FileInputStream fi = new FileInputStream("al.ser");
+      ObjectInputStream oi = new ObjectInputStream(fi);
+      // cast til arraylist af generisk type
+      outputArrayList = (ArrayList<E>) oi.readObject();
+      // luk inputstreams og returner
+      oi.close();
+      fi.close();
+      return outputArrayList;
+   }
 
-   public static void main(String args[]) {
+   public static void main(String args[]) throws IOException, ClassNotFoundException {
       
-      // Initialize ArrayLists
+      // Initialize ArrayLists - must be of reference type objects - not primitive types
       ArrayList<Integer> intArrayList = new ArrayList<Integer>();
       ArrayList<String> stringArrayList = new ArrayList<String>();
       
@@ -25,11 +54,15 @@ public class GenericMethod {
          stringArrayList.add("Nummer " + i);
       }
       
-      System.out.println("\nArrayList of Integer objects");
+      System.out.println("ArrayList of Integer objects");
       printArrayList(intArrayList);
+      System.out.println();
       
-      System.out.println("\nArrayList of String objects");
+      System.out.println("ArrayList of String objects");
       printArrayList(stringArrayList);
+      System.out.println();
       
+      writeArrayList(intArrayList);
+      System.out.println(readArrayList());
    }
 }
